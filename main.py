0e45750d9931cd4703b3e4114b84b7c2c0cf3025
Pierre Jacquet <44034432+jacquetpi@users.noreply.github.com>
2023-09-08 16:15:40 +0200

    partial load

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from utils.cpuexplorer import CpuExplorer
 delay = 15
 
 cmd_tsk = 'taskset -c {subset}'
-cmd_load = 'stress-ng --cpu {core} --timeout ' + str(delay)
+cmd_load = 'stress-ng --cpu {core} -l {len} --timeout ' + str(delay)
 
 cpuset = CpuExplorer().build_cpuset()
 allocated_cpu = [cpuset.get_cpu_list()[0]]
@@ -18,18 +18,38 @@ while len(allocated_cpu) != len(cpuset.get_cpu_list()):
 load_levels = range(1,len(allocated_cpu)+1)
 with open("exp-def.sh", "w") as f:
     f.write('#!/bin/bash\n')
-    for level in load_levels:
-        gen_cmd = cmd_load.replace('{core}', str(level))
-        f.write(gen_cmd + '\n')
+    f.write('results=\'results-def\'\n')
+    f.write('program=\'/usr/bin/python3 /usr/local/src/rapl-reader/rapl-reader.py\'\n')
+    f.write('mkdir "$results"\n')
+    for stress_level in range(10,101,10):
+        f.write('#Stress level ' + str(stress_level) + '\n')
+        f.write('$program &\n')
+        f.write('app_pid=$!\n')
+        for load_level in load_levels:
+            req_core = round(1/(stress_level/100)*load_level)
+            gen_cmd = cmd_load.replace('{core}', str(req_core)).replace('{len}', str(stress_level))
+            f.write(gen_cmd + '\n')
+        f.write('kill $app_pid\n')
+        f.write('mv consumption.csv "$results"/consumption-' + str(stress_level) + '%\n')
 
 tasksets = [x.get_cpu_id() for x in allocated_cpu]
 duration = 0
 with open("exp-smt.sh", "w") as f:
     f.write('#!/bin/bash\n')
-    for level in load_levels:
-        duration+=delay
-        subset = ''.join(str(core) + ',' for core in tasksets[:level])[:-1]
-        gen_cmd = cmd_tsk.replace('{subset}', subset) + ' ' + cmd_load.replace('{core}', str(level))
-        f.write(gen_cmd + '\n')
+    f.write('results=\'results-smt\'\n')
+    f.write('program=\'/usr/bin/python3 /usr/local/src/rapl-reader/rapl-reader.py\'\n')
+    f.write('mkdir "$results"\n')
+    for stress_level in range(10,101,10):
+        f.write('#Stress level ' + str(stress_level) + '\n')
+        f.write('$program &\n')
+        f.write('app_pid=$!\n')
+        for load_level in load_levels:
+            req_core = round(1/(stress_level/100)*load_level)
+            duration+=delay
+            subset = ''.join(str(core) + ',' for core in tasksets[:load_level])[:-1]
+            gen_cmd = cmd_tsk.replace('{subset}', subset) + ' ' + cmd_load.replace('{core}', str(req_core)).replace('{len}', str(stress_level))
+            f.write(gen_cmd + '\n')
+        f.write('kill $app_pid\n')
+        f.write('mv consumption.csv "$results"/consumption-' + str(stress_level) + '%\n')
 
 print('Total duration', duration, 'sec i.e.', float(duration/60), 'mn')
